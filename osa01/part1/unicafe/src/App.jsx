07d79035dc3b4c8d258510f9592c689665a2916d
Atte <atte.ra12@gmail.com>
2024-08-04 18:17:03 +0300

    1.10

--- osa01/part1/unicafe/src/App.jsx
+++ osa01/part1/unicafe/src/App.jsx
@@ -1,49 +1,87 @@
-import { useState } from 'react'
-
-
+import { useState } from 'react';
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-)
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  // const [allRatings, setAll] = useState([])
-  const handleGood = () => {
-    // setAll(allRatings.concat('G'))
-    setGood(good + 1)
-    console.log(good)
+);
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <p>{text}: {value}</p>
+  );
+}
+
+
+const Statistics = ({ good, neutral, bad, allRatings }) => {
+  const total = good + neutral + bad;
+  // const average = total === 0 ? 0 : allRatings / total;
+  let average;
+  if (total === 0) {
+    average = 0;
+  } else {
+    average = allRatings / total;
   }
-  
-  const handleBad = () => {
-    // setAll(allRatings.concat('R'))
-    setBad(bad + 1)
-    console.log(bad)
+  let posPercentage;
+  if (total === 0) {
+    posPercentage = 0;
+  } else {
+    posPercentage = good / total * 100;
   }
-  
-  const handleNeutral = () => {
-    // setAll(allRatings.concat('R'))
-    setNeutral(neutral + 1)
-    console.log(neutral)
+
+  if (total === 0) {
+    return <div>No feedback given</div>;
   }
-  
 
   return (
     <div>
-      <h1> Give feedback </h1>
-        <Button handleClick={handleGood} text='Good' />
-        <Button handleClick={handleBad} text='Bad' />
-        <Button handleClick={handleNeutral} text='Neutral' />
-        <h2>Statistics</h2>
-        <p> Good: {good}</p>
-        <p> Bad: {bad}</p>
-        <p> Neutral: {neutral}</p>
-    </div>
-  )
-}
+      <StatisticsLine text="good" value ={good} />
+      <StatisticsLine text="neutral" value ={neutral} />
+      <StatisticsLine text="bad" value ={bad} />
+      <StatisticsLine text="all" value ={total} />
+      <StatisticsLine text="average" value ={average.toFixed(1)} />
+      <StatisticsLine text="positive" value ={posPercentage.toFixed(1)} />
 
-export default App
+      {/* <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average.toFixed(1)}</p>
+      <p>Positive: {posPercentage.toFixed(1)}%</p> */}
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allRatings, setAll] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(allRatings + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(allRatings - 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGood} text='Good' />
+      <Button handleClick={handleBad} text='Bad' />
+      <Button handleClick={handleNeutral} text='Neutral' />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} allRatings={allRatings} />
+    </div>
+  );
+};
+
+export default App;
