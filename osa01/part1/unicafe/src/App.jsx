dafcd917b8f50a53aa02aa66d79ac73fca3ca26a
Atte <atte.ra12@gmail.com>
2024-08-04 18:41:08 +0300

    1.11*, 1.12*

--- osa01/part1/unicafe/src/App.jsx
+++ osa01/part1/unicafe/src/App.jsx
@@ -52,11 +52,32 @@ const Statistics = ({ good, neutral, bad, allRatings }) => {
   );
 };
 
+const GetAnecdote = ({ anecdote }) => {
+  return (
+    <p>{anecdote}</p>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [allRatings, setAll] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const handleNextAnecdote = () => {
+    const newIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(newIndex);
+  };
 
   const handleGood = () => {
     setGood(good + 1);
@@ -78,8 +99,12 @@ const App = () => {
       <Button handleClick={handleGood} text='Good' />
       <Button handleClick={handleBad} text='Bad' />
       <Button handleClick={handleNeutral} text='Neutral' />
+      <Button handleClick={handleNeutral} text='Next anecdote' />
       <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} allRatings={allRatings} />
+      <Button handleClick={handleNextAnecdote} text='Next anecdote' />
+      <GetAnecdote anecdote={anecdotes[selected]} />
+
     </div>
   );
 };
